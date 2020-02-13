de4a387ade07f506de27b1cfcdbc69d440afb4f3
vintasoft <D:\The Bat!>
2020-02-13 12:37:58 +0300

    Source codes update

--- CSharp/VintasoftImagingLicense.cs
+++ CSharp/VintasoftImagingLicense.cs
@@ -1,19 +1,19 @@
-﻿namespace Vintasoft
+﻿namespace VintasoftImaging
 {
     /// <summary>
     /// Provides information about VintaSoft Imaging.NET SDK Evaluation License.
     /// </summary>
-    public class VintasoftImagingLicense
+    internal class VintasoftImagingLicense
     {
 
         /// <summary>
         /// Registers VintaSoft Imaging.NET SDK Evaluation License.
         /// </summary>
-        public static void Register()
+        internal static void Register()
         {
             // ==========================================
             // Register the evaluation version of VintaSoft Imaging .NET SDK:
-            // Vintasoft.Imaging.ImagingGlobalSettings.Register(user, email, expirationDate, regCode);
+            Vintasoft.Imaging.ImagingGlobalSettings.Register("Alex", "alex@vintasoft.com", "2020-03-11", "BJXp5VSmvZiiis5Tqf+sqpWwSI5iebv7el2ZbAB3jv1yosWj5K9OOC6uop8PQwW06ybTDKdwQO50oy+bz7pX/uUZdJcgUQyAM57HMYQ5o5GG01CIxKhHDSKb2XcTr3mBYsUAaM3r2CSXF3/NIJrV44MhOHHFmANv/m5Qs4RT7js");
             // ==========================================            
         }
 
